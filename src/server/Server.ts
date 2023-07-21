99a28604d0b5f7f4218a6ed945fa53b8d271d9c7
Yuri Guilherme <yuri.luana2016@gmail.com>
2023-07-21 12:34:32 -0300

    class #5

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 
+import { router } from "./routes";
+
 // Criar uma instância básica do servidor
 const server = express();
 
-server.get("/", (_, res) => {
-  return res.send("Olá, DEV!");
-});
+server.use(express.json());
+
+server.use(router);
 
 
 // exportar a instãncia do servidor
