b1ac1b266c9973656cf2ba45d335e91a7cb9bf8d
Yuri Guilherme <yuri.luana2016@gmail.com>
2023-08-01 12:08:45 -0300

    fix: resolve production problem

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import { server } from "./server/Server";
+import { Knex } from "./server/database/knex";
 
 // A porta que irá rodar a API Node
-server.listen(process.env.PORT || 3333, () => {
-  console.log(`App rodando na porta ${process.env.PORT || 3333}`);
-});
+const startServer = () => {
+  server.listen(process.env.PORT || 3333, () => {
+    console.log(`App rodando na porta ${process.env.PORT || 3333}`);
+  });
+};
+
+
+if (process.env.IS_LOCALHOST !== "true") {
+  Knex.migrate.latest()
+    .then(() => {
+      startServer();
+    })
+    .catch(console.log)
+  ;
+} else {
+  startServer();
+}
