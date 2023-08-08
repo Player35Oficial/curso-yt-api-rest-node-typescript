158c0361d2c4a9f717ee278966556bf5f9f14b5e
Yuri Guilherme <yuri.luana2016@gmail.com>
2023-08-08 14:39:16 -0300

    feat: added jwt generation and updated middleware and controllers

--- src/server/controllers/usuarios/SignIn.ts
+++ src/server/controllers/usuarios/SignIn.ts
@@ -5,7 +5,7 @@ import { validation } from "../../shared/middleware";
 import { StatusCodes } from "http-status-codes";
 import { IUsuario } from "../../database/models";
 import { UsuariosProvider } from "../../database/providers/usuarios";
-import { PasswordCrypto } from "../../shared/services";
+import { JWTService, PasswordCrypto } from "../../shared/services";
 
 interface IBodyProps extends Omit<IUsuario , "id" | "nome" > { }
 
@@ -20,9 +20,9 @@ export const signIn = async (req: Request<{}, {}, IBodyProps >, res: Response) =
 
   const { email, senha } = req.body;
   
-  const result = await UsuariosProvider.getByEmail(email);
+  const usuario = await UsuariosProvider.getByEmail(email);
 
-  if (result instanceof Error) {
+  if (usuario instanceof Error) {
     return res.status(StatusCodes.UNAUTHORIZED).json({
       errors: {
         default: "Email ou senha inválidos"
@@ -30,7 +30,7 @@ export const signIn = async (req: Request<{}, {}, IBodyProps >, res: Response) =
     });
   }
 
-  const passwordMatch = await PasswordCrypto.verifyPassword(senha, result.senha);
+  const passwordMatch = await PasswordCrypto.verifyPassword(senha, usuario.senha);
 
   if (!passwordMatch) {
     return res.status(StatusCodes.UNAUTHORIZED).json({
@@ -39,6 +39,22 @@ export const signIn = async (req: Request<{}, {}, IBodyProps >, res: Response) =
       }
     });
   } else {
-    return res.status(StatusCodes.OK).json({ accessToken: "teste.teste.teste" });
+
+    const accessToken = JWTService.sign({ uid: usuario.id });
+
+    if (accessToken === "JWT_SECRET_NOT_FOUND") {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        errors: {
+          default: "Erro ao gerar o token de acesso"
+        }
+      });
+    }
+
+
+
+
+
+
+    return res.status(StatusCodes.OK).json({ accessToken });
   }
 };
